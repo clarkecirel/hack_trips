412b22ee730a5d4ae8b7e105b062e65f9ed3e915
Clarke Cirel <Clarke.cirel@gmail.com>
2019-12-07 16:29:24 -0700

    third model added

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,60 @@
 class AddressesController < ApplicationController
+  before_action :set_location
+  before_action :set_address, only: [:show, :edit, :update, :destroy]
+  
   def index
-  end
-
-  def new
+    @addresses = @location.address
   end
 
   def show
   end
 
-  def edit
+  def new
+    @address = Address.new
   end
+  
+  def create
+    @address = @address.locations.new(address_params)
+    if @address.save
+      redirect_to trip_location_path(@location.trip_id, @location)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    binding.pry
+    if @address.update(address_params)
+      redirect_to location_addresses_path(@location)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @address.destroy
+    redirect_to location_address_path(@location)
+  end
+  
+  private
+    def set_location
+      @location = Location.find(params[:location_id])
+    end
+
+    def set_address
+      @address = @location.address
+    end
+
+    def address_params
+      params.require(:address).permit(:city, :state, :street, :zip)
+    end  
 end
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root 'trips#index'
 
   resources :trips do
-    resources :locations, only: [:index, :new, :create, :destroy, :show]
+    resources :locations, only: [:index, :new, :create, :destroy, :show, :edit]
   end
   
   resources :locations do
-    resources :addresses, only: [:index, :new, :create, :destroy, :show ]
+    resources :addresses, only: [:index, :new, :create, :destroy, :show, :edit ]
   end
 
 end
