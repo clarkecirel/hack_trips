97c9714fa142d65d9e56bd66d5a11cf138b32c7c
Clarke Cirel <Clarke.cirel@gmail.com>
2019-12-07 17:03:37 -0700

    final

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root 'trips#index'
 
   resources :trips do
-    resources :locations, only: [:index, :new, :create, :destroy, :show, :edit]
+    resources :locations, only: [:index, :new, :create, :destroy, :show, :edit, :update]
   end
   
   resources :locations do
-    resources :addresses, only: [:index, :new, :create, :destroy, :show, :edit ]
+    resources :addresses, only: [:index, :new, :create, :update, :destroy, :show, :edit ]
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
-name = ['street' ,'city', 'zip', 'state']
 10.times do 
-  name = Address.create(
-    street: Faker::Address.street_address 
-    city: Faker::Address.city
-    state: Faker::Address.state
+  Address.create(
+    street: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
     zip: Faker::Address.zip
   )
-
 end
-  # 10.times do
     
